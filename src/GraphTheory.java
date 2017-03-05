856a4d44d61eb64a611d86631d2d95f442579727
Sean <minhson293@gmail.com>
2017-03-05 21:34:26 +0700

    Read text file and store into a matrix

--- src/GraphTheory.java
+++ src/GraphTheory.java
@@ -1,33 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 public class GraphTheory {
     static int n, m, matrix[][];
-    public static void inputMatrix(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter #rows: ");
-        n = input.nextInt();
-        System.out.println("Enter #columns: ");
-        m = input.nextInt();
-        matrix = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print("  a["+i+","+j+"]=");
-                matrix[i][j] = input.nextInt();
+    public static int[][] readMatrixFile(String fileName, int n){
+        File file = new File(fileName);
+        int[][] temp = new int[n][n];
+        int i=0;
+        try {
+            Scanner input = new Scanner(file);
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                List<String> numbers = Arrays.asList(line.split(","));
+                int j = 0;
+                for (String number:numbers){
+                    temp[i][j] = Integer.valueOf(number);
+                    j++;
+                }
+                i++;
+            }
+            input.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return temp;
+    }
+
+
+
+    public static void main(String[] args) {
+  //      inputMatrix();
+   //     exportMatrix();
+          matrix = readMatrixFile("test.txt", 3);
+        //print the matrix out
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-    }
-
-    public static void exportMatrix(){
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j] +"  ");
-            }
-            System.out.println("");
-        }
-    }
-
-    public static void main(String[] args) {
-        inputMatrix();
-        exportMatrix();
     }
 }
