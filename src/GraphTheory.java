506db2e5c104e3bd3af87055aef0a0b26bf1f973
Sean <minhson293@gmail.com>
2017-04-09 23:35:19 +0700

    ưtf

--- src/GraphTheory.java
+++ src/GraphTheory.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 public class GraphTheory {
-    static int n, m, matrix[][];
+    static int n, m, matrix[][], edges[];
     public static int[][] readMatrixFile(String fileName, int n){
         File file = new File(fileName);
         int[][] temp = new int[n][n];
@@ -29,19 +29,203 @@ public class GraphTheory {
         }
         return temp;
     }
-
-
-
-    public static void main(String[] args) {
-  //      inputMatrix();
-   //     exportMatrix();
-          matrix = readMatrixFile("test.txt", 3);
-        //print the matrix out
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
+    /**1. Degree of a matrix **/
+    private static int degreeCal(int[][] A, int x){
+        int sum = 0;
+        for (int i=0; i < A[x].length; i++){
+            sum = sum + A[x][i];
         }
+        if (A[x][x] == 1)
+            return sum + 1;
+        else
+            return sum;
+    }
+    private static int[] degree(int[][] A, int n){
+        int[] temp = new int[n];
+        for (int count =0; count < A.length; count++){
+            temp[count] = degreeCal(A,count);
+        }
+        return temp;
+    }
+    /**2. Path existance**/
+    private static boolean pathExistance(int[][] A, int[] visited, int x, int y){
+        visited[x] = 1;
+        if (A[x][y] >=1) return true;
+        for (int i = 0; i < A.length; i++){
+            if ((visited[i]==0)&&(A[x][i]!=0)){
+                if (pathExistance(A, visited, i,y)==true) return true;
+            }
+        }
+        return false;
+    }
+    private static void pathExistanceResult(boolean result,int x, int y){
+        if (result == true) System.out.println("There exists a path between vertrice " + x + " and " + y);
+        if (result == false) System.out.println("There is no path between vertrice " + x + " and " + y);
+    }
+    /**3. Almost Euler**/
+    private static boolean checkDuplicate(int[][] A,int i,int j){
+        for (int k=0; k< A.length; k++){
+            if (A[k][0] == j && A[k][1] ==i) return true;
+        }
+        return false;
+    }
+    private static int[][] readEdges(int[][] A){
+        int[][] Edges = new int[A.length*2][2];
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                if (A[i][j] ==1 && !checkDuplicate(Edges,i,j)){
+                    Edges[count][0] = i;
+                    Edges[count][1]= j ;
+                    count++;
+                }
+            }
+        }
+        return Edges;
+    }
+    private static int[] Almost_euler(int[][] A, int v){
+        int[][] Edges = readEdges(A);
+        int[] P = new int[A.length+1];
+        int w = 0, edge = 0;
+        P[0] = v;
+        int counter = 1;
+        boolean Flag = false; // False is for fail, true everything is going well
+        //Find in Edges an edge that start in v
+        for (int i =0; i < Edges.length; i++){
+            if (Edges[i][0]==v){
+                w = Edges[i][1];
+                edge = i;
+                Flag = true;
+                break;
+            }
+        }
+//        print2DMatrix(Edges);
+//        Edges[Edges.length - 1][0] = -1;
+        // Add the vertex w to P
+        P[counter] = w;
+//        System.out.println(counter);
+        counter+=1;
+        //Remove the edge
+        Edges[edge][0] = -1;
+        // To define the conditions
+        while(P[counter-1]!=v){
+            Flag = false;
+            w=P[counter-1];
+            //Find in Edges and edge that start with w
+            for (int i =0; i < Edges.length; i++){
+                if (Edges[i][0]==w){
+                    w = Edges[i][1];
+                    edge = i;
+                    Flag = true;
+                    break;
+                }
+            }
+            if (Flag==false){
+                counter = counter -1;
+                if (counter<0) return P;
+
+            }
+            else {
+                P[counter]=w;
+                counter +=1;
+                Edges[edge][0]=-1;
+            }
+        }
+        return P;
+    }
+    private static boolean check_Almost_euler_result(int[] P){
+        if (P[0] == P[P.length-1]){
+            return true;
+        }
+        else return false;
+    }
+
+    private static void Almost_euler_result(int[] P){
+        if (P[0] == P[P.length-1]){
+            System.out.println("The Euler Path: ");
+            printMatrix(P);
+        }
+        else System.out.println("There is no Euler path");
+    }
+    //    /**4. Euler circuit**/
+    public static int[] Euler_circuit(int[][] A){
+        int v = 0;
+        int[] P = new int[0];
+        int[][] Edges = readEdges(A);
+        if (check_Almost_euler_result(Almost_euler(A,v))) P = Almost_euler(A,v);
+        while (!check_Euler(P,Edges)){
+            Edges = removeEdges(P,Edges);
+
+        }
+        return P;
+    }
+    private static int[][] edgesTo_adjacency(int[][] Edges){
+        int[][] A = new int[Edges.length][Edges.length];
+        int count = 0;
+//        for (int i = 0; i < A.length; i++) {
+//            for (int j = 0; j < A[i].length; j++) {
+//                if (A[i][j] ==1 && !checkDuplicate(Edges,i,j)){
+//                    Edges[count][0] = i;
+//                    Edges[count][1]= j ;
+//                    count++;
+//                }
+//            }
+//        }
+        for (int i=0;i<Edges.length;i++){
+
+        }
+        return Edges;
+    }
+    private static int[][] removeEdges(int[] P,int[][] Edges){
+        for (int i=0;i<P.length;i++){
+            for (int j=0;j<Edges.length;j++){
+                if (P[i] == Edges[j][0] && P[i+1] == Edges[j][1]) Edges[j][0] = -1;
+            }
+        }
+        return Edges;
+    }
+    private static boolean check_Euler(int[] P, int[][] Edges){
+        if (P.length == Edges.length){
+            return true;
+        }
+        else return false;
+    }
+
+    public static void printMatrix(int[] A){
+        for (int i=0; i<A.length;i++) System.out.print(A[i] + " ");
+    }
+    public static void print2DMatrix(int[][] A){
+        //print matrix out
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        matrix = readMatrixFile("testEuler.txt", 6);
+        int[] visited = new int[100];
+        int[] result;
+        int[][] result2;
+
+//        System.out.println(matrix[1][2]);
+        // Run degree calculation
+//        int result = degreeCal(matrix,0);
+//        System.out.println(result);
+        /**1.Code to run degree calculation, it prints of an array of degree of each vertex **/
+//        result = degree(matrix,6);
+//        printMatrix(result);
+        /**2. Run path existance algorithm**/
+//        pathExistance(matrix,visited,0,4);
+//        pathExistanceResult(pathExistance(matrix,visited,0,4),0,4);
+        //test readEdges
+//            result2 = readEdges(matrix);
+//            System.out.println(result2.length);
+//            print2DMatrix(result2);
+//        //3.Almost_Euler
+//        result = Almost_euler(matrix,1);
+//        Almost_euler_result(result);
+
+
     }
 }
